58ff31feafdf1a4e6e778a1cfdc722c52021dfc4
YouYou94 <ybh4115@gmail.com>
2023-07-10 21:47:59 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from 'components';
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Button>추가</Button>
+    </div>
+  );
 }
 
 export default App;
